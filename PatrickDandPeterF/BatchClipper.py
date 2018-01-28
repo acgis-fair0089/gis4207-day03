35a7f04bd965b78e23639b876bec756602e014cb
MCE-SMM-ADVGIS\MCE <drap0051@algonquinlive.com>
2018-01-27 19:39:30 -0500

    Tool works need to change name of the file and load Arcpy at proper time

---
+++ PatrickDandPeterF/BatchClipper.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      MCE
+#
+# Created:     23/01/2018
+# Copyright:   (c) MCE 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+# Name: Clip_Example2.py
+# Description: Clip major roads that fall within the study area.
+
+# Import system modules
+import arcpy
+import os
+import sys
+from arcpy import env
+
+
+Clipfeature = "../../../Data/BatchClipData/TargetData"
+Clipingfeature = "../../../Data/BatchClipData/Sites"
+
+
+
+arcpy.env.workspace = Clipfeature
+Cliplist = arcpy.ListFeatureClasses()
+
+arcpy.env.workspace = Clipingfeature
+ClipingList = arcpy.ListFeatureClasses()
+
+OuputWorkspace = "../../../Data/BatchClipData/OuputFolder"
+
+for x in Cliplist:
+    for y in ClipingList:
+        arcpy.Clip_analysis(Clipfeature +"/" + x, Clipingfeature +"/" + y, OuputWorkspace +"/"+x[:-4]+y)
+
+
+
+
+
+
