b7151b06f86a24750b98ac4316266571f29de103
MCE-SMM-ADVGIS\MCE <drap0051@algonquinlive.com>
2018-01-29 10:24:35 -0500

    Now working

--- PatrickDandPeterF/BatchClipper.py
+++ PatrickDandPeterF/BatchClipper.py
@@ -13,28 +13,33 @@
 # Description: Clip major roads that fall within the study area.
 
 # Import system modules
-import arcpy
-import os
 import sys
+import os
+
+
+if len(sys.argv) != 4:
+    print "Usage: BatchClipper.py <InWorkspace> <ClipWorkspace> <OutputWorkspace>"
+    sys.exit()
+
+import arcpy
 from arcpy import env
 
 
-Clipfeature = "../../../Data/BatchClipData/TargetData"
-Clipingfeature = "../../../Data/BatchClipData/Sites"
+InWorkspace = sys.argv[1]
+ClipWorkspace = sys.argv[2]
+OuputWorkspace = sys.argv[3]
 
 
-
-arcpy.env.workspace = Clipfeature
+arcpy.env.workspace = InWorkspace
 Cliplist = arcpy.ListFeatureClasses()
 
-arcpy.env.workspace = Clipingfeature
+arcpy.env.workspace = ClipWorkspace
 ClipingList = arcpy.ListFeatureClasses()
 
-OuputWorkspace = "../../../Data/BatchClipData/OuputFolder"
 
 for x in Cliplist:
     for y in ClipingList:
-        arcpy.Clip_analysis(Clipfeature +"/" + x, Clipingfeature +"/" + y, OuputWorkspace +"/"+y[:-4]+"_"+x)
+        arcpy.Clip_analysis(InWorkspace +"/" + x, ClipWorkspace +"/" + y, OuputWorkspace +"/"+y[:-4]+"_"+x)
 
 
 
